89175a2673c445723d54aaab22ca68b18173e7f0
chulwoong park <chulwoongpark@chulwoongui-MacBook-Pro.local>
2017-10-20 12:31:59 +0900

    secret my key

--- main.go
+++ main.go
@@ -7,22 +7,15 @@ import (
 	"net/http"
 
 	"github.com/stretchr/gomniauth"
-	"github.com/stretchr/gomniauth/providers/facebook"
-	"github.com/stretchr/gomniauth/providers/github"
-	"github.com/stretchr/gomniauth/providers/google"
 )
 
 func main() {
 	// gomniauth.SetSecurityKey("PUT YOUR KEY")
 	gomniauth.SetSecurityKey("woong")
 	gomniauth.WithProviders(
-		//facebook.New("key", "secret", "http://localhost:8080/auth/callback/facebook	"),
-		//github.New("key", "secret", "http://localhost:8080/auth/callback/github"),
-		//google.New("key", "secret", "http://localhost:8080/auth/callback/google"),
-		facebook.New("key", "secret", "http://localhost:8080/auth/callback/facebook"),
-		github.New("key", "secret", "http://localhost:8080/auth/callback/github"),
-		google.New("1085126168381-t281rb019442a0g486aerkdvg964q8fd.apps.googleusercontent.com",
-			"6YQN9fKlDfTnvp7wyIcGfe3v", "http://localhost:8080/auth/callback/google"),
+	//facebook.New("key", "secret", "http://localhost:8080/auth/callback/facebook	"),
+	//github.New("key", "secret", "http://localhost:8080/auth/callback/github"),
+	//google.New("key", "secret", "http://localhost:8080/auth/callback/google"),
 	)
 	http.Handle("/login", &example.TemplateHandler{Filename: "login.html"})
 	http.Handle("/default", auth.MustAuth(&example.TemplateHandler{Filename: "default.html"}))
